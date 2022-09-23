b3edf609951af23e9ead7730d81104fae069aefa
ashwini_basavaraju <ashwini_basavaraju@cms.co.in>
2022-09-23 15:31:20 +0530

    commit

---
+++ Controllers/MedicalRepresentativeScheduleController.cs
@@ -0,0 +1,119 @@
+﻿using CSPharmacyMedicineSupplyManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using CSMedicalRepresentativeScheduleMicroservice.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CSMedicalRepresentativeScheduleMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MedicalRepresentativeScheduleController : ControllerBase
+    {
+        private readonly PharmacyMedicineSupplyManagementContext db;
+
+        public MedicalRepresentativeScheduleController(PharmacyMedicineSupplyManagementContext _db)
+        {
+            db = _db;
+        }
+
+
+        [HttpGet]
+        [Route("RepScheduler")]
+        public async Task<List<RepSchedule>> RepSchedule(DateTime scheduleStartDate)
+        {
+            List<RepSchedule> returnRepScheduleList = new List<RepSchedule>();
+            List<MedicalRep> medicalRepList = new List<MedicalRep>();
+            List<Doctor> doctorList = new List<Doctor>();
+            MedicalRep medicalRep = new MedicalRep();
+            Doctor doctor = new Doctor();
+
+            try
+            {
+                medicalRepList = medicalRep.getMedicalReps();
+                doctorList = doctor.getDoctors();
+
+
+                string Baseurl = "https://localhost:44318/";
+                List<MedicineStock> medicineStockList = new List<MedicineStock>();
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url  
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                    HttpResponseMessage Res = await client.GetAsync("api/MedicineStock");
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var ProductResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        medicineStockList = JsonConvert.DeserializeObject<List<MedicineStock>>(ProductResponse);
+
+                        int startDate = 0;
+                        int index = 0;
+                        foreach (var item in doctorList)
+                        {
+                            RepSchedule repSchedule = new RepSchedule();
+                            repSchedule.Doctorname = item.DoctorName;
+                            repSchedule.DoctorContactNumber = item.DoctorContactNumber;
+                            repSchedule.MeetingSlot = "1 PM - 2 PM";
+                            if (medicalRepList.Count == index)
+                            {
+                                index = 0;
+                            }
+
+                            repSchedule.Name = medicalRepList[index].MedicalRepName;
+
+                            repSchedule.TreatingAilment = item.TreatingAilment;
+                            foreach (var medicineItem in medicineStockList)
+                            {
+                                if (medicineItem.TargetAilment == item.TreatingAilment)
+                                {
+                                    repSchedule.Medicine = medicineItem.MedicineName;
+                                }
+                            }
+
+                            if (scheduleStartDate.AddDays(startDate).DayOfWeek == DayOfWeek.Sunday)
+                            {
+                                startDate++;
+                            }
+
+                            repSchedule.DateOfMeeting = scheduleStartDate.AddDays(startDate);
+                            returnRepScheduleList.Add(repSchedule);
+                            startDate++;
+                            index++;
+                        }
+
+                        return returnRepScheduleList;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Models/RepSchedule.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSMedicalRepresentativeScheduleMicroservice.Models
+{
+    //public class RepSchedule
+    //{
+    //    public string MedicalRepresentativeName { get; set; }
+    //    public string DoctorName { get; set; }
+    //    public string MeetingSlot { get; set; }
+    //    public DateTime DateOfMeeting { get; set; }
+    //    public string DoctorContactNumber { get; set; }
+
+    //}
+
+    public class MedicalRep
+    {
+        public int MedicalRepId { get; set; }
+        public string MedicalRepName { get; set; }
+        public string MedicalRepConatactNo { get; set; }
+
+        List<MedicalRep> medicalReps = new List<MedicalRep>();
+        public MedicalRep()
+        {
+
+        }
+
+        public MedicalRep(int MedicalRepId, string MedicalRepName, string MedicalRepConatactNo)
+        {
+            this.MedicalRepId = MedicalRepId;
+            this.MedicalRepName = MedicalRepName;
+            this.MedicalRepConatactNo = MedicalRepConatactNo;
+        }
+
+        public List<MedicalRep> getMedicalReps()
+        {
+            medicalReps.Add(new MedicalRep(101, "john", "9999999999"));
+            medicalReps.Add(new MedicalRep(102, "jack", "9999999998"));
+            medicalReps.Add(new MedicalRep(103, "anne", "9999999994"));
+            return medicalReps;
+        }
+    }
+
+   
+
+    public class Doctor
+    {
+        public int DocId { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorContactNumber { get; set; }
+        public string TreatingAilment { get; set; }
+
+
+        List<Doctor> doctors = new List<Doctor>();
+        public Doctor()
+        {
+
+        }
+
+        public Doctor(int DocId, string DoctorName, string DoctorContactNumber, string TreatingAilment)
+        {
+            this.DocId = DocId;
+            this.DoctorName = DoctorName;
+            this.DoctorContactNumber = DoctorContactNumber;
+            this.TreatingAilment = TreatingAilment;
+        }
+
+        public List<Doctor> getDoctors()
+        {
+            doctors.Add(new Doctor(11, "john", "9999999999","General"));
+            doctors.Add(new Doctor(12, "Eras", "9999999998", "Gynaecology"));
+            doctors.Add(new Doctor(13, "Penn", "9999999997", "Orthopaedics"));
+            doctors.Add(new Doctor(14, "Fruity", "9999999996", "General"));
+            doctors.Add(new Doctor(15, "Gingy", "9999999995", "Gynaecology"));
+            doctors.Add(new Doctor(16, "Penny", "9999999994", "Orthopaedics"));
+            return doctors;
+        }
+
+    }
+}
+
