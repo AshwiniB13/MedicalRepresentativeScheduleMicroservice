96b1384f43c19a952946a62d29788b4d87d31646
ashwini_basavaraju <ashwini_basavaraju@cms.co.in>
2022-09-23 18:30:21 +0530

    commit

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,7 +31,7 @@ namespace MedicalRepresentativeScheduleMicroservice
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<CSPharmacyMedicineSupplyManagement.Models.PharmacyMedicineSupplyManagementContext>();
+            services.AddDbContext<CSPharmacyMedicineSupplyManagement.Models.PharmacyMedicineSupplyManagementContext>(c => c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
   .AddJwtBearer(options =>
